0a6f44e6a72a75fcb91ccc4c02db51d754c0cf20
marc <marc@der-wu.de>
2010-10-09 20:19:12 +0200

    DEUTSCH: - Letzte kleine ?nderungen f?r den TS3 viewer.
    ENGLISCH:
    - Last small changes for TS3 viewer.

--- inc/community/community.teamspeak.php
+++ inc/community/community.teamspeak.php
@@ -3,14 +3,14 @@ title($_LANG['COMMUNITY']['TEAMSPEAK']);
 ?>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="/teamspeak/tsstatus.css" />
-<script type="text/javascript" src="/teamspeak/tsstatus.js"></script>
+<link rel="stylesheet" type="text/css" href="teamspeak/tsstatus.css" />
+<script type="text/javascript" src="teamspeak/tsstatus.js"></script>
 </head>
 <body>
 <center><?php
 require_once("teamspeak/tsstatus.php");
 $tsstatus = new TSStatus($SETTING['TS3IP'], $SETTING['TS3QPORT'], $SETTING['TS3SID']);
-$tsstatus->imagePath = "/teamspeak/img/";
+$tsstatus->imagePath = "teamspeak/img/";
 $tsstatus->showNicknameBox = $SETTING['TS3SHOWAC'];
 $tsstatus->showPasswordBox = $SETTING['TS3SHOWPW'];
 $tsstatus->decodeUTF8 = true;
